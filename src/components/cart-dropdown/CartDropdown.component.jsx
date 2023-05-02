e1e45bfed1a155f93554306fc9e15700f5f8007d
joeebw <joeegaarciia@gmail.com>
2023-05-02 12:26:14 -0600

    modifying the dropdown style

--- src/components/cart-dropdown/CartDropdown.component.jsx
+++ src/components/cart-dropdown/CartDropdown.component.jsx
@@ -2,9 +2,11 @@ import Button from '../buttons/Button.component'
 
 function CartDropdown() {
   return (
-        <div className="absolute z-20 right-11 top-16 card w-72 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h1>This is the card product</h1>
+        <div className="absolute z-20 right-11 top-16 card w-64 min-h-[300px] bg-base-100 shadow-xl">
+          <div className="card-body justify-between">
+            <div className='pt-9'>
+              <h1>Your card is empty</h1>
+            </div>
             <Button 
               selectButton='inverted'
             >
