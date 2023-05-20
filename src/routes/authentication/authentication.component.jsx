e8b6deb2fdbef6841193c077d3bc37e8bdb3b670
joeebw <joeegaarciia@gmail.com>
2023-05-19 22:38:39 -0600

    navigate to the root page if there is user in authenthication page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "../../store/user/user.selector"
 import SignUpForm from "../../components/sign-up form/SignUpForm.component"
 import SignIn from "../../components/sign-in form/SignIn.component"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Authentication() {
+  const user = useSelector(selectCurrentUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!user) return; 
+    navigate('/');
+  },[user])
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-16 flex-grow mt-9 '>
       <SignIn/>
